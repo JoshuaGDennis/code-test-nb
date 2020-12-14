428550b5df4a4b8c3a5a9461484d4fcc7590d835
JoshDennis <joshuagardennis@gmail.com>
2020-12-14 16:42:05 +0000

    some basic css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => (
-  <div className="container p-2">
+  <div className="container p-2 m-auto bg-white rounded shadow mt-10">
     <h1>THIS IS THE TITLE</h1>
   </div>
 )
